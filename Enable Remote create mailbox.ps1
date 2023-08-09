55c6c8653ec0ecf3cd6e7569e49a1d8c8648ab43
0xDEEDOXD <141853434+0xdeedoxd@users.noreply.github.com>
2023-08-09 22:56:02 +0700

    Enable Remote create mailbox.ps1

--- Enable Remote create mailbox.ps1
+++ Enable Remote create mailbox.ps1
@@ -1,2 +1,3 @@
-Enable Remote create mailbox 
+#Enable Remote create mailbox 
+#please change "user principal name to user that you want to create ex. elonmusk@xxx.com"
 Enable-RemoteMailbox -Identity "User principalname" -RemoteRoutingAddress "user principalname.mail.onmicrosoft.com"
